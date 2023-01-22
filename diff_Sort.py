954d6b127da45db5690220a14d155324552b3282
vinceedesu <55903157+vinceedesu@users.noreply.github.com>
2023-01-22 23:25:30 +0800

    insertion sort done

--- diff_Sort.py
+++ diff_Sort.py
@@ -37,5 +37,15 @@ def bubble_sort(nums):
             print(nums)
         print(nums)
     
-bubble_sort(nums)
+def insertion_sort(nums):
+    for i in range(1,len(nums)):
+        j = i
+        while nums[j - 1 ] > nums[j] and j > 0:
+            nums[j - 1], nums[j] = nums[j], nums[j-1]
+            j -= 1
+            
+        print(nums)
+
+insertion_sort(nums)
+
 
