e187a110eefc480d3b933fbc3c46fd08e180a643
vinceedesu <55903157+vinceedesu@users.noreply.github.com>
2023-01-22 21:42:00 +0800

    selection sort tutorial

---
+++ selection-Sort.py
@@ -0,0 +1,28 @@
+# Pls also do the tutorial for the ff algorithm:
+# Selection Sort
+# Bubble Sort
+# Insertion Sort
+# Merge Sort
+# Quick Sort
+
+# Selection Sort
+
+# nums = [80, 68, 10, 31, 85, 67, 53, 100, 82, 19]
+
+def selection_sort(nums):
+    
+    for i in range(5):
+        minPosition = i
+        for j in range(i,6):
+            if nums[j] < nums[minPosition]:
+                minPosition = j
+    
+    temp = nums[i]
+    nums[i] = nums[minPosition]
+    nums[minPosition] = temp
+    
+    print(nums)
+
+nums = [80, 68, 10, 31, 85, 67]
+selection_sort(nums)
+# print(nums)
