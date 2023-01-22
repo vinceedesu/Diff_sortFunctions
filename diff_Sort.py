47ac81a5288038d82aedcd9f33b6896b9a4345b1
vinceedesu <55903157+vinceedesu@users.noreply.github.com>
2023-01-22 22:59:17 +0800

    Bubble sort done

--- diff_Sort.py
+++ diff_Sort.py
@@ -24,7 +24,18 @@ def selection_sort(nums):
         print(nums)
     print(nums)
 
-selection_sort(nums)   
+ 
+#### Buble Sort #####
+def bubble_sort(nums):
+    for i in range(len(nums)-1,0,-1):
+        for j in range(i):
+            if nums[j] > nums[j + 1]:
+                temp = nums[j]
+                nums[j] = nums[j + 1]
+                nums[j + 1] = temp
     
+            print(nums)
+        print(nums)
     
+bubble_sort(nums)
 
